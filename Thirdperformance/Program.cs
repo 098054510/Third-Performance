4f0e95846380b689cdf4a7330975e8a251cae9a4
Eduardo de Souza <eduardohsfreitas86@gmail.com>
2018-11-06 07:37:36 -0300

    Third Performance/Program made in C#

---
+++ Thirdperformance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Thirdperformance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int VolumeInicial, Vazao, Tempo, VolumeFinal;
+
+            VolumeInicial = int.Parse(Console.ReadLine());
+            Vazao = int.Parse(Console.ReadLine());
+            Tempo = int.Parse(Console.ReadLine());
+
+            if(Vazao * Tempo > VolumeInicial)
+            {
+                Console.WriteLine("Isto é fisicamente Impossivel!");
+            }
+            else
+            {
+                VolumeFinal = VolumeInicial - Vazao * Tempo;
+                Console.WriteLine(VolumeFinal);
+            }
+            Console.ReadLine();
+        }
+    }
+}
